c424e4d535e808ba2329412f2b63618ac2ec1b66
seyoungChu <seyoung@aceproject.co.kr>
2020-04-16 13:20:24 +0900

    [데모] 실행 오류 수정 중.

---
+++ fc02Test/Assets/1.Scripts/UI/SliderHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FC;
+using UnityEngine.UI;
+
+namespace FC
+{
+    public class SliderHandler : MonoBehaviour
+    {
+        private Slider slider;
+        private ThirdPersonOrbitCam cam;
+
+        public void Start()
+        {
+            cam = Camera.main.GetComponent<ThirdPersonOrbitCam>();
+            slider = GetComponent<Slider>();
+            float speed = cam.horizontalAimingSpeed * 10f;
+            transform.Find("value").GetComponent<Text>().text = speed.ToString();
+            slider.value = speed;
+            //Adds a listener to the main slider and invokes a method when the value changes.
+            slider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        }
+
+        public void ValueChangeCheck()
+        {
+            float value = slider.value;
+            cam.horizontalAimingSpeed = cam.verticalAimingSpeed = value / 10f;
+            PlayerPrefs.SetFloat("aimingSpeed", value);
+            transform.Find("value").GetComponent<Text>().text = value.ToString();
+        }
+    }
+}
+
