acaf6c6b5a5f1b49bae896ceea3ee38d8688d54f
seyoungChu <seyoung@aceproject.co.kr>
2020-04-28 08:35:24 +0900

    [강의] 핸들 테스트 스크립트

---
+++ fc02Test/Assets/Test/Editor/SceneHandleEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SceneHandle))]
+public class SceneHandleEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        SceneHandle handle = target as SceneHandle;
+        Handles.color = Color.cyan;
+        Transform handleTR = handle.transform;
+        Handles.DrawLine(handleTR.position, handleTR.position + handleTR.forward * handle.Size);
+        Handles.color = Color.magenta;
+        Handles.DrawLine(handleTR.position, handleTR.position + handleTR.right * handle.Size);
+        Handles.color = Color.green;
+        Handles.DrawLine(handleTR.position, handleTR.position + handleTR.up * handle.Size);
+        Handles.color = Color.white;
+        Handles.DrawWireArc(handleTR.position,-handleTR.up,handleTR.right, 90f,handle.Size);
+        Handles.color = Color.yellow;
+        Handles.DrawWireArc(handleTR.position, handleTR.up , -handleTR.right,90f, handle.Size);
+
+    }
+}
---
+++ fc02Test/Assets/Test/SceneHandle.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneHandle : MonoBehaviour
+{
+    public float Size = 1f;
+}
