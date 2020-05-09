feeaaaa07c9b073e1e1ab248de13bc7738bf1fab
seyoung <seyoung@aceproject.co.kr>
2020-05-10 00:20:34 +0900

    [list] 수정

--- fc02Test/Assets/1.Scripts/GameData/EffectList.cs
+++ fc02Test/Assets/1.Scripts/GameData/EffectList.cs
@@ -5,12 +5,4 @@ using System.Collections.Generic;
 public enum EffectList
 {
 	None = -1,
-	
-    bulletHole = 0,
-    enemyHealth = 1,
-    flash = 2,
-    hitEffect = 3,
-    sparks = 4,
-    tracer = 5,
-
 }
