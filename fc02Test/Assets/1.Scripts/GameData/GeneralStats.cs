ca368fe1ae00a12f25cd9c916324f81ffdc40b8a
주세영 <seyoung@aceproject.co.kr>
2020-06-08 17:14:42 +0900

    [주석추가.]

--- fc02Test/Assets/1.Scripts/GameData/GeneralStats.cs
+++ fc02Test/Assets/1.Scripts/GameData/GeneralStats.cs
@@ -7,34 +7,34 @@ namespace FC
     public class GeneralStats : ScriptableObject
     {
         [Header("General")]
-        [Tooltip("NPC patrolling speed (clear state).")]
+        [Tooltip("NPC 정찰 속도(clear state).")]
         public float patrolSpeed = 2f;
-        [Tooltip("NPC search speed (warning state).")]
+        [Tooltip("NPC 검색 속도 (warning state).")]
         public float chaseSpeed = 5f;
-        [Tooltip("NPC evade speed (engage state).")]
+        [Tooltip("NPC 회피 속도 (engage state).")]
         public float evadeSpeed = 15f;
-        [Tooltip("How long to wait on a waypoint.")]
+        [Tooltip("웨이포인트에서 대기시간.")]
         public float patrolWaitTime = 2f;
-        [Tooltip("The obstacle layer mask.")]
+        [Tooltip("장애물 레이어마스크")]
         public LayerMask obstacleMask;
         [Header("Animation")]
-        [Tooltip("Clearance angle (deadzone) to avoid aim flickering.")]
+        [Tooltip("조준시 깜빡임을 피하기위한 확정 앵글(deadzone)")]
         public float angleDeadzone = 5f;
-        [Tooltip("Damping time for the speed parameter.")]
+        [Tooltip("속도 댐핑 시간.")]
         public float speedDampTime = 0.4f;
-        [Tooltip("Damping time for the angularSpeed parameter.")]
+        [Tooltip("각속도 댐핑 시간.")]
         public float angularSpeedDampTime = 0.2f;
-        [Tooltip("Response time for turning an angle into angularSpeed.")]
+        [Tooltip("각속도 안에서 각도 회전에 대한 반응 시간")]
         public float angleResponseTime = 0.2f;
         [Header("Cover")]
-        [Tooltip("Low cover height to consider crouch when taking cover.")]
+        [Tooltip("장애물에 숨었을때 고려해야할 최소 높이값.")]
         public float aboveCoverHeight = 1.5f;
-        [Tooltip("The cover layer mask.")]
+        [Tooltip("장애물 레이어 마스크 The cover layer mask.")]
         public LayerMask coverMask;
         [Header("Shoot")]
-        [Tooltip("Layer mask to cast shots.")]
+        [Tooltip("사격 레이어마스크 Layer mask to cast shots.")]
         public LayerMask shotMask;
-        [Tooltip("Layer mask of target(s).")]
+        [Tooltip("타겟 레이어마스크 Layer mask of target(s).")]
         public LayerMask targetMask;
     }
 
